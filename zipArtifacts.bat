c2fbc79d9a2d47ac3bf83e4d56af7e3e6b8a1e9d
takke <hiroaki@v7.com>
2019-04-30 22:11:39 +0900

    add zipArtifacts.bat

---
+++ zipArtifacts.bat
@@ -0,0 +1,16 @@
+@echo off
+
+set OUTFILE_INST=hoge-Installer.zip
+set OUTFILE_EXE=hoge-Exe.zip
+
+echo AAA > %OUTFILE_EXE%
+echo BBB > %OUTFILE_INST%
+
+
+@echo start generate MD5 hash
+certutil -hashfile %OUTFILE_EXE% MD5  | find /v "MD5" | find /v "CertUtil" > %OUTFILE_EXE%.md5
+certutil -hashfile %OUTFILE_INST% MD5 | find /v "MD5" | find /v "CertUtil" > %OUTFILE_INST%.md5
+@echo end generate MD5 hash
+
+
+exit /b 0
